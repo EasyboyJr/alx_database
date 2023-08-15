4fc8868aa43a3987ae1aa45712fc921022c8a353
George <georgeoluwasemilore1@gmail.com>
2023-08-15 10:47:55 +0100

    test

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, COLUMN_TYPE; IS_NULLABLE, COLUMN_DEFAULT, EXTRA
-FROM INFORMATION_SCHEMA,COLUMNS
-WHERE TABLE_NAME = 'first_table';
+DESC first_table;
