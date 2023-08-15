c6a072cec0c83aa5222bde109f707ac2e145bc26
George <georgeoluwasemilore1@gmail.com>
2023-08-15 11:16:46 +0100

    test

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- A script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SHOW FULL COLUMNS
-FROM first_table;
+SHOW FULL COLUMNS FROM first_table;
