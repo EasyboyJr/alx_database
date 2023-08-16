7a3de915b9b7a9d76c466d3a4b7868e45f9fb621
George <georgeoluwasemilore1@gmail.com>
2023-08-16 11:17:51 -0700

    added not null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 --  a script that creates the table id_not_null on your MySQL server.
 CREATE TABLE IF NOT EXISTS id_not_null(
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY NOT NULL,
 	name VARCHAR(256)
 	);
