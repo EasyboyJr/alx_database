a764b731a44701ef459f9d99dcfccffbf3786dfd
George <georgeoluwasemilore1@gmail.com>
2023-08-16 17:03:12 +0100

    added a script that list all privileges of a user

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- A script that list all the of the mysql user user_0d_1 and user_0d_2 
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- and for the second user
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
