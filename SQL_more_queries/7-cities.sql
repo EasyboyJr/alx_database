b07416f63207510424a3d373bee81cbce5e71057
Onifade George Oluwasemilore <113639398+EasyboyJr@users.noreply.github.com>
2023-08-17 09:07:25 +0100

    Update 7-cities.sql

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,8 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- use the newely created database
 USE hbtn_0d_usa;
--- CREATE THE TABLE
 CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL
